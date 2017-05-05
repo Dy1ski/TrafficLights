366a0fe50a7dfee7eb5b472c0b5ddd71a7c84caf
Dy1ski <dylanjames.n@hotmail.com>
2017-05-05 18:43:36 +1000

    Add files via upload

---
+++ TrafficState.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class TrafficState - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum TrafficState
+{
+    Green_NorthSouthTraffic, Amber_NorthSouthTraffic, Red_NorthSouthTraffic, Green_EastWestTraffic, Amber_EastWestTraffic, Red_EastWestTraffic
+}
